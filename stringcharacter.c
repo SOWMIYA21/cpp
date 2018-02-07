2089823debe646ed9757748b9c53aa896180ccf1
SOWMIYA21 <35294707+SOWMIYA21@users.noreply.github.com>
2018-02-07 16:02:36 +0530

    cpp
    character

---
+++ stringcharacter.c
@@ -0,0 +1,15 @@
+#include<iostream.h>
+void main()
+{
+int spaces;
+cout<<"Enter your full name:">>;
+cin.getline(input 45);
+spaces=0;
+do
+{
+if((input[45=='']))
+spaces++;
+}
+while(strlen(input)!=0);
+strcpy(output[45],input[0]);
+}
